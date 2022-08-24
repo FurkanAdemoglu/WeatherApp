6c2cdfd6385d3b786c02e1c3b3c8d8ff14270028
Furkan Ademoglu <furkanademoglu69@gmail.com>
2022-08-24 15:49:50 +0300

    application is created and aded to manifes navgraph and bottomnavigation is code

--- KafeinWeatherApp/app/src/main/java/com/example/kafeinweatherapp/MainActivity.kt
+++ KafeinWeatherApp/app/src/main/java/com/example/kafeinweatherapp/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.kafeinweatherapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val bottomNav: BottomNavigationView = findViewById(R.id.bottomNavigationView)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        val navController: NavController = navHostFragment.navController
+
+        bottomNav.setupWithNavController(navController)
     }
 }
